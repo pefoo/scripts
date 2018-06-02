6d36b9c45d47e4eeecec41402a55de5e0c9a5954
peepe <pepus.halt@gmail.com>
2018-06-02 17:19:00 +0200

    added first setup script - vim_plugins

---
+++ setups/vim_plugins.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+#
+# ale
+#
+mkdir -p ~/.vim/pack/ale/start
+git clone https://github.com/w0rp/ale.git ~/.vim/pack/ale/start
+
+#
+# completor
+#
+mkdir -p ~/.vim/pack/completor/start
+git clone https://github.com/maralla/completor.vim.git ~/.vim/pack/completor/start
+
+#
+# gruvbox
+#
+git clone https://github.com/morhetz/gruvbox.git ~/.vim/pack/default/start/gruvbox
+
+#
+# airline
+#
+git clone https://github.com/morhetz/gruvbox.git ~/.vim/pack/default/start/gruvbox
+
+

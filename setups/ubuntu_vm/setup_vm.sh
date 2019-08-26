f3019a0cffbc7199f958f8e5da67b0ef89c734a5
peepe <pepus@gmail.com>
2019-08-26 16:50:59 +0200

    updated vm installation to new dot file linker

--- setups/ubuntu_vm/setup_vm.sh
+++ setups/ubuntu_vm/setup_vm.sh
@@ -1,8 +1,33 @@
 #!/bin/bash
 
+#
+# Setup a vm 
+#   - Update applications
+#   - Install applications (see $applications)
+#   - link dot files 
+#
+
+# functions required to link dot files 
+source ../link_dotfiles.sh -i
+
 function log_console {
   echo -e "\e[93m${1}\e[39m"
 }
+
+
+# Get git repo passwordd 
+prompt_git_pw
+
+# Get user password 
+echo -en "\e[92mEnter your user password:\e[39m "
+read -s upwd
+
+# Create local git config 
+make_gitconfig_local
+
+# update applications
+echo $upwd | sudo -S apt install update
+echo $upwd | sudo -S apt install upgrade 
 
 # applications to install 
 applications="
@@ -12,10 +37,11 @@ applications="
 "
 
 log_console "Installing applications $applications"
-sudo apt install -y $applications
+echo $upwd | sudo -S apt install -y $applications
 
 log_console "Installing vim plugins"
 source "../../vim/install_plugins.sh"
 
-source ../dot_files.sh
+# Link the dot files 
+link_dot_files
 

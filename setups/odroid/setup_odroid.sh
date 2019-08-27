bbaa211d6c120ef5c2622a970323194b568ffa45
peepe <pepus.halt@gmail.com>
2019-08-27 15:46:03 +0200

    setup odroid

---
+++ setups/odroid/setup_odroid.sh
@@ -0,0 +1,28 @@
+#! /bin/bash
+
+#
+# Fresh odroid setup (yet untested)
+#   - update / install apps 
+#   - setup Intenso nfs export 
+#
+
+this_path=$(dirname $(realpath $0))
+source "$this_path/../../functions/log.sh"
+source "$this_path/../../functions/assert_run_as_root.sh"
+
+assert_run_as_root 
+
+# Update exsting software 
+log_msg "Updating software"
+apt-get -qq update
+apt-get -qq -y upgrade
+
+# Install software 
+software="openssh-server monit nfs-kernel-server ntfs-3g vim"
+log_msg "Installing software: $software"
+apt-get -y -qq install $software
+
+# Setup nfs
+sh "$this_path/../create_ngs_export.sh Intenso"
+
+

e2ce1978b5c4e72695a41731e058a924ed0104f2
peepe <pepus.halt@gmail.com>
2019-09-10 16:29:58 +0200

    first menu items for the setup wrapper

--- setups/install_gnome_extensions.sh
+++ setups/install_gnome_extensions.sh
@@ -10,76 +10,97 @@
 # Once installed, restart the gnome shell (alt-f2 -> r). The new extensions will appear in gnome tweak tools
 #
 
+while getopts "i" o; do
+  case "$o" in
+    i)
+      # This file was sourced from some interactive scrip. Skip actual execution
+      interactive_mode=true
+      ;;
+  esac
+done
+
 readonly EXTENSION_SITE="https://extensions.gnome.org"
 readonly EXTENSION_FOLDER="$HOME/.local/share/gnome-shell/extensions"
 
-readonly THIS_PATH=$(dirname $(realpath $0))
+[ -d "$0" ] && readonly THIS_PATH="$0" || readonly THIS_PATH=$(dirname $(realpath $0))
 source "$THIS_PATH/../functions/log.sh"
 
 # Extension IDs taken from https://extensions.gnome.org
 declare -Ar EXTENSIONS=(
   # https://EXTENSIONS.gnome.org/extension/1317/alt-tab-switcher-popup-delay-removal/
-  [alt_tab_switcher]=1317
+  [Alt tab switcher]=1317
   # https://extensions.gnome.org/extension/755/hibernate-status-button/
-  [hibernate_button]=755
+  [Hibernate button]=755
   # https://extensions.gnome.org/extension/1267/no-title-bar/
-  [no_title_bar]=1267
+  [No title bar]=1267
   # https://extensions.gnome.org/extension/19/user-themes/
-  [user_themes]=19
+  [User themes]=19
   # https://extensions.gnome.org/extension/484/workspace-grid/
-  [workspace_grid]=484
+  [Workspace grid]=484
   # https://extensions.gnome.org/extension/1066/fix-multi-monitors/
-  [multi_monitor_fix]=1066
+  [Multi monitor fix]=1066
   # https://extensions.gnome.org/extension/120/system-monitor/
-  [system_monitor]=120
+  [System monitor]=120
 )
 
 # Install extension prerequisites 
-log_msg "Installing extension prerequisites"
-sudo apt install -y -qq gir1.2-gtop-2.0 gir1.2-networkmanager-1.0 gir1.2-clutter-1.0
+# Args:
+#   NONE
+function install_prerequisites() {
+  log_msg "Installing extension prerequisites"
+  sudo apt install -y -qq  gir1.2-gtop-2.0 gir1.2-networkmanager-1.0  gir1.2-clutter-1.0
+}
 
-# Get gnome shell version 
-readonly GNOME_VERSION=$(gnome-shell --version | grep -oP '\d\.\d{2}')
-if [ -z "$GNOME_VERSION" ];then 
-  log_error "Failed to get gnome shell version"  
+# Install gnome extension 
+# Args
+#   *) The extension ID(s) to install 
+function install_extensions {
+  # Get gnome shell version 
+  readonly GNOME_VERSION=$(gnome-shell --version | grep -oP '\d\.\d{2}')
+  if [ -z "$GNOME_VERSION" ];then 
+    log_error "Failed to get gnome shell version"  
+  fi
+
+  # Iterate over extensions and install them 
+  for extension in "$@"; do
+    extension_params=$(wget -O- "${EXTENSION_SITE}/extension-info/?pk=${extension}&shell_version=$GNOME_VERSION" 2>&1 || grep -i "failed\|error"; exit ${PIPESTATUS[0]})
+    if [ "$?" -ne 0 ];then 
+      log_error "Failed to get the extension parameters"
+      log_error "$extension_params"
+      exit 1
+    fi
+
+    extension_uuid=$(grep -oP '(?<=uuid":\s").*?(?=")' <<< "$extension_params")
+    if [ -z "$extension_uuid" ];then
+      log_error "Failed to extract the extension uuid."
+      exit 1
+    fi
+
+    download_url=$(grep -oP '(?<=download_url":\s").*?(?=")' <<< "$extension_params")
+    if [ -z "$download_url" ];then
+      log_error "Failed to extract the download url."
+      exit 1
+    fi
+    ext_path="${EXTENSION_FOLDER}/${extension_uuid}"
+    
+    if [ -d "$ext_path" ];then
+      log_warn "The extension $extension_uuid is already installed in $ext_path"
+      continue
+    fi
+
+    mkdir -p "$ext_path"
+    wget -qO ext.zip "${EXTENSION_SITE}${download_url}"
+    if [ "$?" -ne 0 ];then 
+      log_error "Failed to download the extension using the url ${EXTENSION_SITE}${download_url}"
+      exit 1
+    fi
+
+    unzip -q ext.zip -d "$ext_path"
+    log_msg "Installed extension $extension_uuid to $ext_path"
+    rm ext.zip
+  done 
+}
+
+if [ ! "$interactive_mode" == true ];then 
+  install_extensions "${EXTENSIONS[@]}"
 fi
-
-# Iterate over extensions and install them 
-for extension in "${!EXTENSIONS[@]}"; do
-  extension_params=$(wget -O- "${EXTENSION_SITE}/extension-info/?pk=${EXTENSIONS[$extension]}&shell_version=$GNOME_VERSION" 2>&1 || grep -i "failed\|error"; exit ${PIPESTATUS[0]})
-  if [ "$?" -ne 0 ];then 
-    log_error "Failed to get the extension parameters"
-    log_error "$extension_params"
-    exit 1
-  fi
-
-  extension_uuid=$(grep -oP '(?<=uuid":\s").*?(?=")' <<< "$extension_params")
-  if [ -z "$extension_uuid" ];then
-    log_error "Failed to extract the extension uuid."
-    exit 1
-  fi
-
-  download_url=$(grep -oP '(?<=download_url":\s").*?(?=")' <<< "$extension_params")
-  if [ -z "$download_url" ];then
-    log_error "Failed to extract the download url."
-    exit 1
-  fi
-  ext_path="${EXTENSION_FOLDER}/${extension_uuid}"
-  
-  if [ -d "$ext_path" ];then
-    log_warn "The extension $extension with the uuid $extension_uuid is already installed in $ext_path"
-    continue
-  fi
-
-  mkdir -p "$ext_path"
-  wget -qO ext.zip "${EXTENSION_SITE}${download_url}"
-  if [ "$?" -ne 0 ];then 
-    log_error "Failed to download the extension using the url ${EXTENSION_SITE}${download_url}"
-    exit 1
-  fi
-
-  unzip -q ext.zip -d "$ext_path"
-  log_msg "Installed extension $extension_uuid to $ext_path"
-  rm ext.zip
-done 
-
---
+++ setups/setup_wrapper.sh
@@ -0,0 +1,137 @@
+#!/bin/bash
+
+#
+# TO BE DONE 
+#
+
+readonly THIS_PATH=$(dirname $(realpath $0))
+source "$THIS_PATH/../functions/log.sh"
+source "$THIS_PATH/../functions/assert_run_as_root.sh"
+
+#
+# Press enter to continue
+#
+function pause() {
+  read -p 'Press [Enter] to continue'
+}
+
+#
+# Implementation of Update packages menu item 
+# Update exsting packes using the package_management script
+#
+function mi_update_packages() {
+  clear 
+  source "$THIS_PATH/package_management.sh"
+  update_packages
+  pause
+}
+
+#
+# Implementation of Install packages menu item 
+# Provide a list of common packages and install the selected ones 
+#
+function mi_install_packages() {
+  selection=$(whiptail --title "Select common packages to install" --checklist \
+    "Use space to select packages" 25 100 16 \
+    "vim"               "Good old vim"                ON \
+    "tmux"              "Terminal multiplexer"        ON \
+    "git"               "Source control"              ON \
+    "openssh-server"    "The open ssh server"         OFF \
+    "monit"             "A log file watcher"          OFF \
+    "fail2ban"          "Ban IPs that fail to login"  OFF \
+    "nfs-kernel-server" "Network file system server"  OFF \
+    "nfs-common"        "Network file system client"  OFF \
+    "ntfs-3g"           "NTFS driver"                 OFF \
+    3>&1 1>&2 2>&3)
+
+  # Cancel 
+  [ "$?" -eq 1 ] && return 0
+  # Nothing was selected 
+  [ -z "$selection" ] && return 0
+
+  clear 
+  source "$THIS_PATH/package_management.sh"
+  # Remove quotes (") from the selection and install the packages 
+  install_packages $(echo "$selection" | tr -d '"')
+  pause
+}
+
+#
+# Implementation of the install gnome extensions menu item
+# Provide a list of common extensions and install the selected ones
+#
+function mi_install_gnome_extensions() {
+  user=$SUDO_USER
+  if [ -z "$user" ];then 
+    clear 
+    log_error "Failed to get the invoking user name. Gnome extensions are installed local."
+    pause
+    return 1
+  fi
+
+  # Run the gnome extension management in a subshell that is owned by the actual user (not root)
+  sudo -H -u "$user" bash -c '
+    source "$0/install_gnome_extensions.sh" -i
+
+    # Build the menu using the extensions provided by the script 
+    c=0
+    for extension in "${!EXTENSIONS[@]}"; do
+      items[$(((c++)))]="${EXTENSIONS[$extension]}"
+      items[$(((c++)))]="$extension"
+      items[$(((c++)))]=ON
+    done
+
+    selection=$(whiptail --title "Select gnome extensions to install" --checklist \
+      "Use space to select extensions" 25 100 16 \
+      "${items[@]}" \
+      3>&1 1>&2 2>&3)
+
+    echo "Selection: $selection"
+    # Cancel 
+    [ "$?" -eq 1 ] && exit 0
+    # Nothing was selected 
+    [ -z "$selection" ] && exit 0
+
+    clear 
+    install_prerequisites
+    install_extensions $(echo "$selection" | tr -d "\"")
+
+  ' "$THIS_PATH"
+  pause
+}
+
+assert_run_as_root
+
+#
+# Menu loop 
+#
+while true; do
+  menu_items[0]="1"; menu_items[1]="Update packages"
+  menu_items[2]="2"; menu_items[3]="Install packages"
+  menu_items[4]="3"; menu_items[5]="Install gnome extensions"
+
+  selection=$(whiptail --title "System setup" --menu "Main menu" --cancel-button "Exit" \
+    25 100 16 "${menu_items[@]}" 3>&1 1>&2 2>&3)
+  ret=$?
+
+  if [ "$ret" -eq 1 ];then 
+    exit 0
+  fi
+  
+  case "$selection" in
+    "1")
+      mi_update_packages
+      ;;
+    "2")
+      mi_install_packages
+      ;;
+    "3")
+      mi_install_gnome_extensions
+      ;;
+    *)
+      clear
+      log_error "How the heck did you get here?!"
+      exit 1;
+  esac
+done
+

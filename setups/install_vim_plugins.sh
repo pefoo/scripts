8bb9fe8cd4fd6a5939a71d6461632dc42655a8df
peepe <pepus.halt@gmail.com>
2019-09-11 13:06:53 +0200

    modified install vim plugin script to be used by other scripts

--- setups/install_vim_plugins.sh
+++ setups/install_vim_plugins.sh
@@ -1,48 +1,56 @@
 #!/bin/bash
 
-function log_console {
-  echo -e "\e[93m${1}\e[39m"
+#
+# Install vim 8 plugins (using the native plugin manager)
+#
+
+while getopts "i" o; do
+  case "$o" in
+    i)
+      # This file was sourced from some interactive scrip. Skip actual execution
+      interactive_mode=true
+      ;;
+  esac
+done
+
+[ -d "$0" ] && readonly THIS_PATH="$0" || readonly THIS_PATH=$(dirname $(realpath $0))
+source "$THIS_PATH/../functions/log.sh"
+
+# List of common plugins 
+declare -Ar PLUGINS=(
+  [ale]="https://github.com/w0rp/ale.git"
+  [completor]="https://github.com/maralla/completor.vim.git"
+  [gruvbox]="https://github.com/morhetz/gruvbox.git"
+  [airline]="https://github.com/vim-airline/vim-airline"
+  )
+
+readonly PLUGIN_BASE_DIR="$HOME/.vim/pack/default"
+
+# Install a single vim plugin using git
+# Args:
+#   1) The plugin name 
+#   2) The source url (most likely a github repo)
+function install_plugin() {
+  local plugin="$1"
+  local source_url="$2"
+
+  if [ -z "$plugin" ] || [ -z "source_url" ]; then 
+    log_error "Provide a plugin name and source url for the vim plugin you want to install"
+  fi
+
+  plugin_dir="${PLUGIN_BASE_DIR}/start/${plugin}"
+  if [ -d "$plugin_dir" ]; then 
+    log_msg "The plugin $plugin is already installed."
+  else
+    mkdir -p "$plugin_dir"
+    git clone -q "${source_url}" "$plugin_dir"
+    log_msg "Installed plugin $plugin"
+  fi
 }
 
-#
-# ale
-#
-if [ ! -d "/home/${USER}/.vim/pack/ale" ]; then
-  log_console "Installing ale"
-  mkdir -p ~/.vim/pack/ale/start
-  git clone https://github.com/w0rp/ale.git ~/.vim/pack/ale/start
-else
-  log_console "Ale is already installed"
-fi
-
-#
-# completor
-#
-if [ ! -d "/home/${USER}/.vim/pack/completor/" ]; then
-  log_console "Installing completor"
-  mkdir -p ~/.vim/pack/completor/start
-  git clone https://github.com/maralla/completor.vim.git ~/.vim/pack/completor/start
-else
-  log_console "Completor is already installed"
-fi
-
-#
-# gruvbox
-#
-if [ ! -d "/home/${USER}/.vim/pack/default/start/gruvbox" ]; then
-  log_console "Installing gruvbox theme"
-  git clone https://github.com/morhetz/gruvbox.git ~/.vim/pack/default/start/gruvbox
-else
-  log_console "Gruvbox is already installed"
-fi
-
-#
-# airline
-#
-if [ ! -d "/home/${USER}/.vim/pack/dist/start/vim-airline" ]; then
-  log_console "Installing airline"
-  git clone https://github.com/vim-airline/vim-airline ~/.vim/pack/dist/start/vim-airline
-else
-  log_console "Airline is already installed"
+if [ ! "$interactive_mode" == true ];then 
+  for plugin in "${!PLUGINS[@]}"; do
+    install_plugin "$plugin" "${PLUGINS[$plugin]}"
+  done 
 fi
 

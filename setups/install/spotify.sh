6961e3c0cce1a44a9b585f9f00379292c1810b16
peepe <pepus.halt@gmail.com>
2020-10-28 12:24:04 +0100

    fixed spotify installation

--- setups/install/spotify.sh
+++ setups/install/spotify.sh
@@ -6,10 +6,8 @@ if command -v spotify > /dev/null; then
   return 0
 fi
 
-# Add spotify repository key 
-sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 931FF8E79F0876134EDDBDCCA87FF9DF48BF1C90
-# Add spotify repository 
-echo deb http://repository.spotify.com stable non-free | sudo tee /etc/apt/sources.list.d/spotify.list
+curl -sS https://download.spotify.com/debian/pubkey_0D811D58.gpg | sudo apt-key add - 
+echo "deb http://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list
 sudo apt update -qq 
 sudo apt install -y spotify-client 
 

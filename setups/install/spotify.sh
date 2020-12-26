9373af90cf59301b39acb5147e46d1ad5a954d15
pefoo <pepus.halt@gmail.com>
2020-12-26 11:13:27 +0100

    fixed typo in spotify install script

--- setups/install/spotify.sh
+++ setups/install/spotify.sh
@@ -11,7 +11,7 @@ if ! command -v curl > /dev/null; then
 fi
 
 curl -sS https://download.spotify.com/debian/pubkey_0D811D58.gpg | sudo apt-key add - 
-cho "deb http://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list
+echo "deb http://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list
 sudo apt-get update -qq && sudo apt-get install -qqy spotify-client
 
 if ! command -v spotify > /dev/null; then 
